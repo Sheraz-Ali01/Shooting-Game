523e24bca2594616529cb6ed0829e228b1758bd0
Sheraz-Ali01 <sheraz71559@gmail.com>
2025-11-15 20:45:07 +0500

    Added Player and Bullets

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@
 #define MaxTree 150
 #define LevelEnimies 12
 #define PlayerSpeed 300.0f
+#define BulletSpeed 700.0
 
 
 
@@ -20,7 +21,7 @@
 typedef struct 
 {
     Vector2 postion;
-    Vector2 speed;
+    Vector2 velocity;
     float health;
     int maxHealth;
     float radius;
@@ -57,7 +58,7 @@ typedef struct {
 
 int main(){
 
-    InitWindow(ScreenWidth, ScreenHeight, "Servival Game");
+    InitWindow(ScreenWidth, ScreenHeight, "Shooting Game");
     SetTargetFPS(60);
 
     srand(time(NULL));
@@ -65,8 +66,8 @@ int main(){
     //Game setup
         //player
         Player player = {0};
-        player.postion= (Vector2){MapWidth,MapHeight};
-        player.speed = (Vector2){0,0};
+        player.postion= (Vector2){MapWidth/2,MapHeight/2};
+        player.velocity = (Vector2){0,0};
         player.health=100;
         player.maxHealth=100;
         player.radius=18;
@@ -103,21 +104,6 @@ int main(){
 
         if(!paused && !gameOver){
 
-                // /// STEP 1: Input (THIS CODE)
-                // Vector2 moveDir = {0, 0};
-                // if (IsKeyDown(KEY_W)) moveDir.y -= 1;  // moveDir = (0, -1)
-
-                // // STEP 2: Normalize (if length > 0)
-                // moveDir = Vector2Normalize(moveDir);   // Still (0, -1) [length was 1]
-
-                // // STEP 3: Set Velocity  
-                // player.velocity = Vector2Scale(moveDir, PLAYER_SPEED);  
-                // // velocity = (0, -1) * 300 = (0, -300)  ← 300 pixels/SECOND UP!
-
-                // // STEP 4: UPDATE POSITION (NEXT in code!)
-                // player.position.x += player.velocity.x * deltaTime;  
-                // player.position.y += player.velocity.y * deltaTime;  
-                // // position.y += -300 * 0.0167 = -5 pixels!  ← MOVES UP!
             Vector2 moveDir={0,0};
             if(IsKeyDown(KEY_W)){
                 moveDir.y-=1;
@@ -131,16 +117,91 @@ int main(){
             if(IsKeyDown(KEY_D)){
                 moveDir.x+=1;
             }
-            if(Vector2lenght(moveDir)>0){
+            if(Vector2Length(moveDir)>0){
                 moveDir=Vector2Normalize(moveDir);
+                player.velocity=Vector2Scale(moveDir,PlayerSpeed);
+
+            }
+            else{
+                player.velocity=Vector2Lerp(player.velocity, (Vector2){0,0}, 12*deltaTime);
+            }
+
+            player.postion.x+=(player.velocity.x)*deltaTime;
+            player.postion.y+=(player.velocity.y)*deltaTime;
+            
+            //Player bound
+            player.postion.x=Clamp(player.postion.x, player.radius, MapWidth-player.radius);
+            player.postion.y=Clamp(player.postion.y, player.radius, MapHeight-player.radius);
+
+            //aim 
+            Vector2 mouseWorld=GetScreenToWorld2D(GetMousePosition(), camera);
+            player.shootDir=Vector2Normalize(Vector2Subtract(mouseWorld, player.postion));
+
+            //window starting
+
+            BeginDrawing();
+            ClearBackground((Color){15, 35, 25, 255}); 
+            BeginMode2D(camera);
+
+
+            // player body
+            Vector2 palpos=player.postion;
+            DrawCircleV(palpos, player.radius, (Color){80, 50, 255, 255 });
+            DrawCircleV(palpos, 14, (Color){120, 180, 255, 255 });
+
+            DrawCircleV(Vector2Add(palpos,player.shootDir), 5, WHITE);
+            DrawCircleV(Vector2Add(palpos, player.shootDir), 3, (Color){50, 100, 200});
+            DrawLineEx(palpos, Vector2Add(palpos,Vector2Scale(player.shootDir,30)),4,ORANGE);
+
+            //Bullet Shooting
+            if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)&& CanShoot){
+                CanShoot=false;
+                for(int i=0; i<MaxBullets; i++){
+                    if(!bullet[i].active){
+                        bullet[i].position=Vector2Add(player.postion, Vector2Scale(player.shootDir,player.radius));
+                        bullet[i].direction=player.shootDir;
+                        bullet[i].active=true;
+                        bullet[i].time=0;
+                        break; //On 1 click only one bullet than exit loop;
+                    }
+                }
+            }
+            if(!IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+                CanShoot=true;
+            }
+            //bullets update time to time
+            for(int i=0; i<MaxBullets; i++){
+                if(bullet[i].active){
+                    bullet[i].position=Vector2Add(bullet[i].position, Vector2Scale(bullet[i].direction, BulletSpeed*deltaTime));
+                    bullet[i].time+=deltaTime;
+
+                    //bullet restrictions/limitations
+                    if(bullet[i].time>2.0f || bullet[i].position.x<0 || bullet[i].position.x>MapWidth || bullet[i].position.y <0 || bullet[i].position.y>MapHeight){
+                        bullet[i].active=false;
+                    }
+
+                }
+            }
+            //Draw bullets
+
+            for(int i=0; i <MaxBullets; i++){
+                if(bullet[i].active){
+                    DrawCircleV(bullet[i].position, 5, YELLOW);
+                    DrawCircleV(bullet[i].position, 10, Fade(YELLOW, 0.3f));
+                }
             }
             
-            BeginDrawing();
-            ClearBackground(BLACK);
+
+
+
+            
+
+
+
+            EndMode2D();
             EndDrawing();
             
         }
-
 
 
 
